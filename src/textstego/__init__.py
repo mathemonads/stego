7e06110f37f8c07041ec1188bfd7371d10dfe696
Oscar Hernandez <oih@hawaii.edu>
2024-05-20 21:34:22 -1000

    initial commit from cookiecutter

---
+++ src/textstego/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for textstego."""
+
+__author__ = """Oscar I. Hernandez"""
+__email__ = 'oscar@sci-mi.org'
+__version__ = '0.1.0'
---
+++ tests/test_textstego.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `textstego` package."""
+
+
+import unittest
+
+from textstego import textstego
+
+
+class TestTextstego(unittest.TestCase):
+    """Tests for `textstego` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
